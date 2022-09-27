1bb33f7022d027cb944a4f1397618197ba95d986
seehar <seehar@qq.com>
2022-09-27 10:31:18 +0800

    init

---
+++ src/components/button/SButton.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: seehar
+ * @Date: 2022-09-27 10:09:14
+ * @LastEditors: seehar
+-->
+<script setup lang="ts"></script>
+
+<template>
+  <button style="color: red">
+    <slot />
+  </button>
+</template>
---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: seehar
+ * @Date: 2022-09-27 10:12:17
+ * @LastEditors: seehar
+ */
+export { default as SButton } from "./button/SButton.vue";
